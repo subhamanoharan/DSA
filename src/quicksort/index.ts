a9ad3ca81aee4e3b7bff1e56da6eccd70b5916df
Subha Manoharan <subham@Subhas-MacBook-Pro.local>
2025-02-20 12:20:08 +0530

    Subha | Spacing

--- src/quicksort/index.ts
+++ src/quicksort/index.ts
@@ -2,7 +2,7 @@ const quickSort = (input: Array<number>): Array<number> => {
     if(input.length < 2) return input
     const [pivot, rest] = input
     let left:Array<number> = [], right: Array<number> =[]
-    for(let i =1; i < input.length;i++) {
+    for(let i = 1; i < input.length;i++) {
         if(input[i]<=pivot)
             left = [...left, input[i]]
         else 
